729f18db74ea6deed392b671a91dc50625af123d
Megan Ma <64246256+meganm0308@users.noreply.github.com>
2022-11-24 00:49:52 -0800

    update code

--- app.R
+++ app.R
@@ -9,41 +9,50 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
-      # Feature 5: Allow user to select multiple types
+      # Feature 1: I added an uiOutput to allow user to select multiple types, 
+      # this is useful since users might want to search all types of liquor.
       uiOutput("typeOutput"),
       uiOutput("countryOutput")
     ),
     mainPanel(
-      # Feature 1: This feature shows the number of results to user, so user
-      # can decide to narrower or wider their filter range
+      # Feature 2: This feature shows the number of results to users, so users
+      # can decide to narrow down or widen their filter range.
       textOutput("filterResult"),
-      # Feature 4: Add more graphs and allow user to select
+      # Feature 3: I added a tabsetpanel and multiple tabpanels to allow users to 
+      # select the specific graph they want to see.
       tabsetPanel(
         tabPanel("Distribution of Alcohol Content", plotOutput("coolplot")),
         tabPanel("Distribution of Sweetness", plotOutput("sweetplot")),
         tabPanel("Distribution of Price", plotOutput("priceplot"))
       ),
-      # Feature 2: This feature allow user to download their filtered results
+      # Feature 4: This feature allows users to download their filtered results;
+      # this is useful because they can bring the downloaded sheet to buy liquors
+      # they want.
       downloadButton("download","Download Results"),
       br(), br(),
-      #Feature 3: This feature allow user to interactive the table.
+      #Feature 5: This feature allows users to interactive the table. This is useful
+      # since users can search the results and choose the number of results showing 
+      # in one page.
       DT::dataTableOutput("results")
     )
   )
 )
 
+# Feature 6: This feature allows users to select multiple countries if they like.
 server <- function(input, output) {
   output$countryOutput <- renderUI({
     selectInput("countryInput", "Country",
                 sort(unique(bcl$Country)),
-                selected = "CANADA")
+                multiple = TRUE,
+                selected = c("CANADA","UNITED STATES OF AMERICA"))
   })  
-  # Feature 5:
+  # Feature 1: I set the default selected to "WINE", because otherwise the graph and result 
+  # will be blank.
   output$typeOutput <- renderUI({
     selectInput("typeInput", "Product Type",
                 sort(unique(bcl$Type)),
                 multiple = TRUE,
-                selected = "WINE")
+                selected = c("WINE","BEER"))
   })  
   
   filtered <- reactive({
@@ -57,11 +66,11 @@ server <- function(input, output) {
     bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
+             Type %in% input$typeInput,
+             Country %in% input$countryInput
       )
   })
-  #Feature 4:
+  #Feature 3: This is the price distribution plot.
   output$priceplot <- renderPlot({
     if (is.null(filtered())) {
       return(NULL)
@@ -70,7 +79,7 @@ server <- function(input, output) {
       geom_histogram() +
       xlab("Price(CAD)")
   })
-  #Feature 4:
+  #Feature 3: This is the sweetness distribution plot.
   output$sweetplot <- renderPlot({
     if (is.null(filtered())) {
       return(NULL)
@@ -88,12 +97,12 @@ server <- function(input, output) {
       geom_histogram() +
       xlab("Alcohol Content")
   })
-  # Feature 3:
+  # Feature 5: I used the DT package to create an interactive table for this app.
   output$results <- DT::renderDataTable({
     filtered()
   })
   
-  # Feature 1:
+  # Feature 4: 
   output$filterResult<-
     renderText({
       numResults <- nrow(filtered())
@@ -103,7 +112,7 @@ server <- function(input, output) {
       paste("We found ", numResults, " options for you!")
     })
   
-  # Feature 2:
+  # Feature 2: I used downloadHandler to allow users to download the result they need.
   output$download <- downloadHandler(
     filename = function() {
       "BC_Liquor_Results.csv"
